90ef7bd067bec8bceb4bcef58e36e41ed191636e
KarinaBertosh <cherrycake246@gmail.com>
2023-07-03 18:08:33 +0300

    Revert "fix: multi"
    This reverts commit 19746b8e71f2b0275344aac3c5e96e8885419539.

--- server.ts
+++ server.ts
@@ -10,18 +10,11 @@ const webpack = require("webpack");
 require("dotenv").config();
 
 const PORT = Number(process.env.PORT) || 5000;
+let clusterPORT = Number(process.env.PORT) || 4000;
+
 const args = process.argv;
-const numCPUs = os.availableParallelism();
-let number = 1;
 
 const server = http.createServer((req: any, res: any) => {
-  if (args.length > 2 && args.at(-1) === "--multi") {
-    const clusterPORT = PORT + number;
-    number += 1;
-    if (number > numCPUs) number = 1;
-    console.log(`Method ${req.method} started on port: ${clusterPORT}`);
-  }
-
   req.users = users;
   switch (req.method) {
     case "GET":
@@ -47,22 +40,29 @@ const server = http.createServer((req: any, res: any) => {
 });
 
 if (args.length > 2 && args.at(-1) === "--multi") {
+  const numCPUs = os.availableParallelism();
+  const arrCpus = new Array(numCPUs);
+  
   if (cluster.isPrimary) {
     for (let i = 0; i < numCPUs; i++) {
-      cluster.fork();
+      arrCpus[i] = cluster.fork({
+        CLUSTER_PORT: clusterPORT + i + 1,
+      });
     }
 
     cluster.on("exit", (worker: any) => {
       console.log(`worker ${worker.process.pid} died`);
-      cluster.fork();
     });
+
+
   } else {
-    console.log(`Server started on port: ${PORT}`);
-    server.listen(PORT);
+    clusterPORT = Number(process.env.CLUSTER_PORT);
+    console.log(`Server ${process.pid} started on port: ${clusterPORT}`);
+    server.listen(clusterPORT);
   }
 } else {
   server.listen(PORT, () => {
-    console.log(`Server started on port: ${PORT}`);
+    console.log(`Server started on port: ${clusterPORT}`);
   });
 }
 
