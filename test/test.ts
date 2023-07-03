5ae5c9edc6ca62e6a80d11501738c450c7d45beb
KarinaBertosh <cherrycake246@gmail.com>
2023-07-03 19:35:54 +0300

    fix: tests

--- test/test.ts
+++ test/test.ts
@@ -27,12 +27,10 @@ async function updateData() {
 
 updateData();
 
-beforeEach(() => {
-  updateData();
-});
-
-
-describe("Test for API", () => {
+describe("First scenario: Post(201), Get/id(200), Put/id(200), Delete/id(204),", () => {
+  beforeEach(() => {
+    updateData();
+  });
   test("POST", async () => {
     const res = await request(server).post("/api/users").send(testUser);
     expect(res.statusCode).toEqual(201);
@@ -59,9 +57,60 @@ describe("Test for API", () => {
   });
 });
 
-describe("Test for API", () => {
+describe("Second scenario: Post(400), Get/id(404), Put/id(404), Delete/id(404),", () => {
+  beforeEach(() => {
+    updateData();
+  });
+
+  test("POST", async () => {
+    const res = await request(server).post("/api/users").send({
+      username: "Karina",
+      age: "22",
+      hobbies: "cycling",
+      f: "ff",
+    });
+    expect(res.statusCode).toEqual(400);
+  });
+
   test("GET/id", async () => {
     const res = await request(server).get(`/api/users/${getUser.id}`);
     expect(res.statusCode).toEqual(404);
+  });
+
+  test("PUT/id", async () => {
+    const res = await request(server)
+      .put(`/api/users/${getUser.id}`)
+      .send(updateUser);
+    expect(res.statusCode).toEqual(404);
+  });
+
+  test("DELETE", async () => {
+    await request(server).post("/api/users").send(testUser);
+    const resDel = await request(server).delete(`/api/users/${getUser.id}`);
+    expect(resDel.statusCode).toEqual(404);
+  });
+});
+
+describe("Third scenario: Get/id(400), Put/id(400), Delete/id(400),", () => {
+  beforeEach(() => {
+    updateData();
+  });
+
+  test("GET/id", async () => {
+    const res = await request(server).get(`/api/users/ggg`);
+    expect(res.statusCode).toEqual(400);
+  });
+
+  test("PUT/id", async () => {
+    const res = await request(server)
+      .put(`/api/users/ggg`)
+      .send(updateUser);
+    expect(res.statusCode).toEqual(400);
+  });
+
+  test("DELETE", async () => {
+    await request(server).post("/api/users").send(testUser);
+    const resDel = await request(server).delete(`/api/users/gggg`);
+    expect(resDel.statusCode).toEqual(400);
   });
 });
